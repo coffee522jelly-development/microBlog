40e3390c7fcf009cdfb8b5aab0c88f5a4f53d741
coffee <coffeejellydev@gmail.com>
2023-08-14 21:46:08 +0900

    ヘッダーを追加

--- app.vue
+++ app.vue
@@ -1,5 +1,7 @@
 <template>
  <NuxtLayout>
+  <header>
+  <!-- Navigation bar -->
   <nav class="flex items-center justify-between flex-wrap bg-teal-500 p-6">
     <div class="flex items-center flex-shrink-0 text-white mr-6">
       <svg class="fill-current h-8 w-8 mr-2" width="54" height="54" viewBox="0 0 54 54" xmlns="http://www.w3.org/2000/svg"><path d="M13.5 22.1c1.8-7.2 6.3-10.8 13.5-10.8 10.8 0 12.15 8.1 17.55 9.45 3.6.9 6.75-.45 9.45-4.05-1.8 7.2-6.3 10.8-13.5 10.8-10.8 0-12.15-8.1-17.55-9.45-3.6-.9-6.75.45-9.45 4.05zM0 38.3c1.8-7.2 6.3-10.8 13.5-10.8 10.8 0 12.15 8.1 17.55 9.45 3.6.9 6.75-.45 9.45-4.05-1.8 7.2-6.3 10.8-13.5 10.8-10.8 0-12.15-8.1-17.55-9.45-3.6-.9-6.75.45-9.45 4.05z"/></svg>
@@ -18,6 +20,34 @@
         </div>
       </div>
   </nav>
+
+  <!-- Hero section with background image, heading, subheading and button -->
+  <div
+    class="relative overflow-hidden bg-cover bg-no-repeat"
+    style="
+      background-position: 50%;
+      background-image: url('https://tecdn.b-cdn.net/img/new/slides/120.webp');
+      height: 350px;
+    ">
+    <div
+      class="absolute bottom-0 left-0 right-0 top-0 h-full w-full overflow-hidden bg-fixed"
+      style="background-color: rgba(0, 0, 0, 0.75)">
+      <div class="flex h-full items-center justify-center">
+        <div class="px-6 text-center text-white md:px-12">
+          <h1 class="mb-6 text-5xl font-bold">microBlog</h1>
+          <h3 class="mb-8 text-3xl font-bold">Let's take a break and write a tech blog.</h3>
+          <!-- <button
+            type="button"
+            class="inline-block rounded border-2 border-neutral-50 px-6 pb-[6px] pt-2 text-xs font-medium uppercase leading-normal text-neutral-50 transition duration-150 ease-in-out hover:border-neutral-100 hover:bg-neutral-500 hover:bg-opacity-10 hover:text-neutral-100 focus:border-neutral-100 focus:text-neutral-100 focus:outline-none focus:ring-0 active:border-neutral-200 active:text-neutral-200 dark:hover:bg-neutral-100 dark:hover:bg-opacity-10"
+            data-te-ripple-init
+            data-te-ripple-color="light">
+            Get started
+          </button> -->
+        </div>
+      </div>
+    </div>
+  </div>
+</header>
     <div class="m-8">
       <NuxtPage />
     </div>
