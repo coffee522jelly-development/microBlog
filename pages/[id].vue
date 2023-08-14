14f93284f704a227e826ebd151f059b125d0d2f1
coffee <coffeejellydev@gmail.com>
2023-08-14 11:59:20 +0900

    レイアウトをカードに変更

--- pages/[id].vue
+++ pages/[id].vue
@@ -1,33 +1,29 @@
 <template>
   <template v-if="data">
-    <h1 class="text-3xl font-semibold">
-      {{ data.title }}
-    </h1>
-    <img
-      :src="data.eyecatch?.url"
-      :width="data.eyecatch?.width"
-      :height="data.eyecatch?.height"
-      alt=""
-      class="mt-6 md:mt-10"
-    />
-    <div
-      class="mt-4 flex flex-col items-start gap-2 md:flex-row md:items-center md:gap-4"
-    >
-      <div
-        class="rounded border-2 border-indigo-600 px-1.5 py-0.5 text-sm font-semibold text-indigo-600"
-      >
-        {{ data.category?.name }}
+      <h1 class="text-3xl font-semibold">
+        {{ data.title }}
+      </h1>
+      <img
+        :src="data.eyecatch?.url"
+        :width="data.eyecatch?.width"
+        :height="data.eyecatch?.height"
+        alt=""
+        class="mt-6 md:mt-10"
+      />
+      <div class="mt-4 flex flex-col items-start gap-2 md:flex-row md:items-center md:gap-4">
+        <div class="rounded border-2 border-indigo-600 px-1.5 py-0.5 text-sm font-semibold text-indigo-600">
+          {{ data.category?.name }}
+        </div>
+        <div class="text-sm text-gray-700">
+          {{ dateFormat(data.publishedAt ?? data.createdAt) }}
+        </div>
       </div>
-      <div class="text-sm text-gray-700">
-        {{ dateFormat(data.publishedAt ?? data.createdAt) }}
-      </div>
-    </div>
     <div v-html="data.content" class="prose mt-6 md:mt-10"></div>
   </template>
 </template>
 
 <script setup lang="ts">
-import { Blog } from "~~/types/blog";
+import { Blog } from "~~/types/Blog";
 
 const { params } = useRoute();
 
--- pages/index.vue
+++ pages/index.vue
@@ -1,15 +1,26 @@
 <template>
-  <ul class="mt-16 grid grid-cols-1 gap-8">
-    <li v-for="blog in data?.contents" :key="blog.id">
-      <NuxtLink :to="`/${blog.id}`" class="flex flex-col gap-4 sm:transition-shadow sm:hover:shadow md:flex-row md:items-center lg:gap-6">
-        <img :src="blog.eyecatch?.url" :width="blog.eyecatch?.width" :height="blog.eyecatch?.height" class="md:w-1/3 md:flex-none" alt="">
-        <div class="md:p-2.5 md:pr-0">
-          <div class="inline-block rounded border-2 border-teal-600 px-1.5 py-0.5 text-sm font-semibold text-teal-600">{{ blog.category?.name }}</div>
-          <div class="mt-2 text-lg font-semibold md:text-xl">{{ blog.title }}</div>
-          <div class="mt-1 text-sm text-gray-700">{{ dateFormat(blog.publishedAt ?? blog.createdAt) }}</div>
+  <ul class="mt-16 grid grid-cols-3 gap-8">
+    <div v-for="blog in data?.contents" :key="blog.id" class="max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+      <NuxtLink :to="`/${blog.id}`">
+        <a href="#">
+            <img :src="blog.eyecatch?.url" class="rounded-t-lg" alt="" />
+        </a>
+        <div class="p-5">
+            <a href="#">
+                <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">{{ blog.title }}</h5>
+            </a>
+            <div class="inline-block rounded border-2 border-teal-600 px-1.5 py-0.5 text-sm font-semibold text-teal-600">{{ blog.category?.name }}</div>
+            <div class="mt-1 text-sm text-gray-700">{{ dateFormat(blog.publishedAt ?? blog.createdAt) }}</div>
+            <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">{{ blog.explain }}</p>
+            <a href="#" class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+                Read more
+                <svg class="w-3.5 h-3.5 ml-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
+                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9"/>
+                </svg>
+            </a>
         </div>
       </NuxtLink>
-    </li>
+    </div>
   </ul>
 </template>
 
