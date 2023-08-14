5748135755954585ef9b6300f0420b5dca3ba582
coffee <coffeejellydev@gmail.com>
2023-08-14 23:53:38 +0900

    ヘッダー、フッターのコンポーネント化

--- app.vue
+++ app.vue
@@ -1,66 +1,21 @@
 <template>
  <NuxtLayout>
-  <header>
-  <!-- Navigation bar -->
-  <nav class="flex items-center justify-between flex-wrap bg-teal-500 p-6">
-    <div class="flex items-center flex-shrink-0 text-white mr-6">
-      <svg class="fill-current h-8 w-8 mr-2" width="54" height="54" viewBox="0 0 54 54" xmlns="http://www.w3.org/2000/svg"><path d="M13.5 22.1c1.8-7.2 6.3-10.8 13.5-10.8 10.8 0 12.15 8.1 17.55 9.45 3.6.9 6.75-.45 9.45-4.05-1.8 7.2-6.3 10.8-13.5 10.8-10.8 0-12.15-8.1-17.55-9.45-3.6-.9-6.75.45-9.45 4.05zM0 38.3c1.8-7.2 6.3-10.8 13.5-10.8 10.8 0 12.15 8.1 17.55 9.45 3.6.9 6.75-.45 9.45-4.05-1.8 7.2-6.3 10.8-13.5 10.8-10.8 0-12.15-8.1-17.55-9.45-3.6-.9-6.75.45-9.45 4.05z"/></svg>
-      <span class="font-semibold text-xl tracking-tight">microBlog</span>
-    </div>
-      <div class="block lg:hidden">
-        <button class="flex items-center px-3 py-2 border rounded text-teal-200 border-teal-400 hover:text-white hover:border-white">
-        <svg class="fill-current h-3 w-3" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><title>Menu</title><path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z"/></svg>
-        </button>
-      </div>
-      <div class="w-full block flex-grow lg:flex lg:items-center lg:w-auto">
-        <div class="text-sm lg:flex-grow">
-        <!--<a href="#responsive-header" class="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white">
-            Blog
-        </a>-->
-        </div>
-      </div>
-  </nav>
-
-    <!-- Hero section with background image, heading, subheading and button -->
-    <div
-      class="relative overflow-hidden bg-cover bg-no-repeat"
-      style="
-        background-position: 50%;
-        background-image: url('https://tecdn.b-cdn.net/img/new/slides/120.webp');
-        height: 350px;
-      ">
-      <div
-        class="absolute bottom-0 left-0 right-0 top-0 h-full w-full overflow-hidden bg-fixed"
-        style="background-color: rgba(0, 0, 0, 0.75)">
-        <div class="flex h-full items-center justify-center">
-          <div class="px-6 text-center text-white md:px-12">
-            <h1 class="mb-6 text-5xl font-bold">microBlog</h1>
-            <h3 class="mb-8 text-3xl font-bold">Let's take a break and write a tech blog.</h3>
-            <!-- <button
-              type="button"
-              class="inline-block rounded border-2 border-neutral-50 px-6 pb-[6px] pt-2 text-xs font-medium uppercase leading-normal text-neutral-50 transition duration-150 ease-in-out hover:border-neutral-100 hover:bg-neutral-500 hover:bg-opacity-10 hover:text-neutral-100 focus:border-neutral-100 focus:text-neutral-100 focus:outline-none focus:ring-0 active:border-neutral-200 active:text-neutral-200 dark:hover:bg-neutral-100 dark:hover:bg-opacity-10"
-              data-te-ripple-init
-              data-te-ripple-color="light">
-              Get started
-            </button> -->
-          </div>
-        </div>
-      </div>
-    </div>
-  </header>
+  <Header></Header>
     <div class="sm:m-32 m-8">
       <NuxtPage />
     </div>
-    <footer class="bg-white rounded-lg shadow m-4 dark:bg-gray-800">
-        <div class="w-full mx-auto max-w-screen-xl p-4 md:flex md:items-center md:justify-between">
-          <span class="text-sm text-gray-500 dark:text-gray-400">© 2023 coffee's home All Rights Reserved.
-          </span>
-          <ul class="flex flex-wrap items-center mt-3 text-sm font-medium text-gray-500 dark:text-gray-400 sm:mt-0">
-              <li>
-                  <a href="#" class="mr-4 hover:underline md:mr-6 ">About</a>
-              </li>
-          </ul>
-        </div>
-    </footer>
+  <Footer></Footer>
  </NuxtLayout>
 </template>
+
+<script>
+import Header from './components/Header';
+import Footer from './components/Footer';
+
+export default {
+  components: {
+    Header,
+    Footer,
+  }
+}
+</script>
