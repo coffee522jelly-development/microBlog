b76784702eaf98ad5dbedcfc7d0b21286ee7aabe
coffee <coffeejellydev@gmail.com>
2023-08-14 15:30:15 +0900

    レスポンシブ

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,5 @@
 <template>
-  <ul class="mt-16 grid grid-cols-3 gap-8">
+  <ul class="mt-16 grid sm:grid-cols-3 grid-cols-1 gap-8">
     <div v-for="blog in data?.contents" :key="blog.id" class="max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
       <NuxtLink :to="`/${blog.id}`">
         <a href="#">
