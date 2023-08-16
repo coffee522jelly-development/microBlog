52e263e869cabd4a5e49126087c438704bc4bee0
coffee <coffeejellydev@gmail.com>
2023-08-16 18:56:47 +0900

    ダークモードの廃止

--- pages/index.vue
+++ pages/index.vue
@@ -1,17 +1,17 @@
 <template>
   <ul class="grid sm:grid-cols-2 grid-cols-1 gap-8 col-span-3 md:col-span-8">
-    <div v-for="blog in data?.contents" :key="blog.id" class="max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+    <div v-for="blog in data?.contents" :key="blog.id" class="max-w-sm bg-white border border-gray-200 rounded-lg shadow">
       <NuxtLink :to="`/${blog.id}`">
         <a href="#">
             <img :src="blog.eyecatch?.url" class="rounded-t-lg" alt="" />
         </a>
         <div class="p-5">
             <a href="#">
-                <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">{{ blog.title }}</h5>
+                <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900">{{ blog.title }}</h5>
             </a>
             <div class="inline-block rounded border-2 border-teal-600 px-1.5 py-0.5 text-sm font-semibold text-teal-600">{{ blog.category?.name }}</div>
             <div class="mt-1 text-sm text-gray-700">{{ dateFormat(blog.publishedAt ?? blog.createdAt) }}</div>
-            <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">{{ blog.explain }}</p>
+            <p class="mb-3 font-normal text-gray-700">{{ blog.explain }}</p>
             <a href="#" class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-teal-700 rounded-lg hover:bg-teal-800 focus:ring-4 focus:outline-none focus:ring-teal-300 dark:bg-teal-600 dark:hover:bg-teal-700 dark:focus:ring-teal-800">
                 Read more
                 <svg class="w-3.5 h-3.5 ml-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
